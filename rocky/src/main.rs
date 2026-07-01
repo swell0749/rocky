343f4d8fb0e2ec3db96083b7b8a03c5d72abc997
swell0749 <swellington0749@gmail.com>
2026-07-01 01:08:05 +0100

    feat(app): add favicon
    Signed-off-by: swell0749 <swellington0749@gmail.com>

--- rocky/src/main.rs
+++ rocky/src/main.rs
@@ -10,8 +10,13 @@ mod logger;
 
 // imports
 use std::error::Error;
-
 use app::App;
+
+// favicon stuff
+static ICON: &[u8] = include_bytes!("../../gh-assets/logo.png");
+
+const ICON_HEIGHT: u32 = 512;
+const ICON_WIDTH: u32 = 512;
 
 // functions
 fn main() -> Result<(), Box<dyn Error>> {
@@ -19,12 +24,40 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     log::debug!("starting rocky...");
 
+    let image = image::load_from_memory(ICON);
+
+    if image.is_err() {
+        log::error!("image not found at 'gh-assets/logo.png': {image:?}");
+
+        return Ok(());
+    }
+
+    // note(swell): should be safe, right?
+    let image = image.unwrap();
+    let icon = iced::window::icon::from_rgba(image.as_bytes().to_vec(), ICON_HEIGHT, ICON_WIDTH);
+
+    if icon.is_err() {
+        log::error!("icon failed to be parsed from rgba file: {icon:?}");
+
+        return Ok(());
+    }
+
+    // note(swell): should be safe, right?
+    let icon = icon.unwrap();
+
+    let window_settings = iced::window::Settings {
+        icon: Some(icon),
+
+        ..Default::default()
+    };
+
     let result = iced::application(
         App::default,
         App::update,
         App::view,
     )
         .title("rocky")
+        .window(window_settings)
         .run();
 
     match result {
